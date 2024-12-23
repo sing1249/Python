20f5e18b6be8b56b03dc2e91c9aa3201c90a4efe
sing1249 <72288523+sing1249@users.noreply.github.com>
2024-12-22 20:06:45 -0500

    Update main.py

--- Project 42 - Simple Age Guess - Flask and Jinja/main.py
+++ Project 42 - Simple Age Guess - Flask and Jinja/main.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     current_year = datetime.now().year
-    random_number = random.randint(1, 10)
     return render_template('index.html', num=random_number, year=current_year)
 
 @app.route("/guess/<name>")
